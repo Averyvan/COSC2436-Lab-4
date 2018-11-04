2737f1e7255daaae7ff62235522102ae82d4667a
Avery <averyvan@gmail.com>
2018-11-04 17:48:02 -0600

    Implemented inFixToPostFix, testing with Infix.txt, and manual entry for infix expressions

--- driver.cpp
+++ driver.cpp
@@ -84,6 +84,82 @@ void postFixEvaluate(string expression, StackInterface<int> *stack)
 	delete stack; //frees memory used by Stack from heap
 } //end postFixEvaluate
 
+/* Converts infix expressions to postfix expressions
+	@pre An unconverted Infix expression
+	@post Infix expression converted to Postfix
+	@param inFix is a string that holds the Infix expression
+	@return the resulting Postfix expression string		*/
+string inFixToPostFix(string inFix)
+{
+	string result;
+	LinkedStack<char> *stack = new LinkedStack<char>;
+	int size = inFix.size();
+	char tempToken;
+	for (int i=0; i<size; i++)
+	{
+		char token = inFix[i];
+		if (token == '#')
+		{
+			result.append("# ");
+		}
+		else if (token == '(')
+		{
+			stack->push('(');
+		}
+		else if (token=='+' || token=='-' || token=='*' || token=='/')
+		{
+			if (stack->isEmpty())
+			{
+				stack->push(token);
+			}
+			else
+			{
+				while (!stack->isEmpty())
+				{
+					tempToken = stack->peek();
+					if (tempToken == '(')
+					{
+						break;
+					}
+					if (token == '*' || token == '/')
+					{
+						if (tempToken == '+' || tempToken == '-')
+						{
+							break;
+						}
+					}
+					result += tempToken;
+					stack->pop();
+				}
+				stack->push(token);
+			}
+		}
+		else if (token == ')')
+		{
+			while (!stack->isEmpty())
+			{
+				tempToken = stack->peek();
+				stack->pop();
+				if (tempToken == '(')
+				{
+					break;
+				}
+				result += tempToken;
+			}
+		}
+		else if (token != ' ') //as long as it's not a space
+		{
+			result += token;
+		}
+	}
+	while (!stack->isEmpty())
+	{
+		result += stack->peek();
+		stack->pop();
+	}
+	return result;
+}
+
 int main ()
 {
 	ifstream inFile;
@@ -100,71 +176,161 @@ int main ()
 	cout<<"LinkedStack Postfix Expression Evaluation: "<<endl;
 	for (int i = 0; i < expressionCount; i++)
 	{ //testing LinkedStack on file
+		cout<<"PostFix Expression: "<<expressionVector[i]<<endl;
 		postFixEvaluate(expressionVector[i], new LinkedStack<int>);
 	}
 	cout<<"\nArrayStack Postfix Expression Evaluation: "<<endl;
 	for (int i = 0; i < expressionCount; i++)
 	{ //testing ArrayStack on file
+		cout<<"PostFix Expression: "<<expressionVector[i]<<endl;
 		postFixEvaluate(expressionVector[i], new ArrayStack<int>);
 	}
+
+	inFile.open("Infix.txt");
+	vector<string> inFixExpressionVector;
+	while (!inFile.eof())
+	{ //fills vector with lines from file
+		getline(inFile, temp);
+		inFixExpressionVector.push_back(temp);
+	}
+	inFile.close();
+	expressionCount = inFixExpressionVector.size();
+	cout<<"\nInfix to Postfix Expression and Calculation: \n";
+	for (int i = 0; i < expressionCount; i++)
+	{
+		cout<<"Infix expression: "<<inFixExpressionVector[i]<<endl;
+		temp = inFixToPostFix(inFixExpressionVector[i]);
+		cout<<"Converted Postfix: "<<temp<<endl;
+		postFixEvaluate(temp, new LinkedStack<int>);
+	}
+	
 	cout<<"\nManual Entry:\n";
 	while (true)
 	{ //loop for user input Postfix Expressions
 		string userInput = " ";
-		cout<<"Enter a Postfix Expression (i.e. 2 3 4 + * ) or \"q\" to quit:\n";
+		cout<<"Enter a Postfix Expression (i.e. 2 3 4 + * ) or q to go to Infix entry:\n";
+		getline(cin, userInput);
+		if (userInput == "q")
+		{ //sentinel exit value
+			cout<<endl;
+			break;
+		}
+		else postFixEvaluate(userInput, new LinkedStack<int>);
+	}
+	while (true)
+	{
+		string userInput = " ";
+		cout<<"Enter an Infix Expression (i.e. 2 3 4 + * ) or q to quit:\n";
 		getline(cin, userInput);
 		if (userInput == "q")
 		{ //sentinel exit value
 			cout<<"Thank you, goodbye!";
 			return 0;
 		}
-		else postFixEvaluate(userInput, new LinkedStack<int>);
+		else
+		{
+			string temp = inFixToPostFix(userInput);
+			cout<<"Converted Postfix: "<<temp<<endl;
+			postFixEvaluate(temp, new LinkedStack<int>);
+		}
 	}
 }
 
-/* Sample output:
-LinkedStack Postfix Expression Evaluation:
-The value is: 473
-The value is: -1
-The value is: 3
-The value is: 3
-The value is: 14
-The value is: 3
-The value is: 14
-Malformed expression - Invalid Operator detected.
-Malformed expression - Invalid Postfix detected.
-Malformed expression - Invalid operand/character detected.
-Malformed expression - Invalid Postfix detected.
-Malformed expression - Invalid Operator detected.
-
-ArrayStack Postfix Expression Evaluation:
-The value is: 473
-The value is: -1
-The value is: 3
-The value is: 3
-The value is: 14
-The value is: 3
-The value is: 14
-Malformed expression - Invalid Operator detected.
-Malformed expression - Invalid Postfix detected.
-Malformed expression - Invalid operand/character detected.
-Malformed expression - Invalid Postfix detected.
-Malformed expression - Invalid Operator detected.
-
-Manual Entry:
-Enter a Postfix Expression (i.e. 2 3 4 + * ) or "q" to quit:
-234+*
-The value is: 14
-Enter a Postfix Expression (i.e. 2 3 4 + * ) or "q" to quit:
-72%
-Malformed expression - Invalid Operator detected.
-Enter a Postfix Expression (i.e. 2 3 4 + * ) or "q" to quit:
-45*2/
-The value is: 10
-Enter a Postfix Expression (i.e. 2 3 4 + * ) or "q" to quit:
-q
-Thank you, goodbye!
---------------------------------
-Process exited after 118.4 seconds with return value 0
-Press any key to continue . . .
-*/
+//	Sample output:
+//		
+//	LinkedStack Postfix Expression Evaluation:
+//	PostFix Expression: 345*+67*8+9*+
+//	The value is: 473
+//	PostFix Expression: 1234*+5/-
+//	The value is: -1
+//	PostFix Expression: 12+43-*
+//	The value is: 3
+//	PostFix Expression: 48+65-*32-22+*/
+//	The value is: 3
+//	PostFix Expression: 234+*
+//	The value is: 14
+//	PostFix Expression: 72/
+//	The value is: 3
+//	PostFix Expression: 512+4*+3-
+//	The value is: 14
+//	PostFix Expression: 72%
+//	Malformed expression - Invalid Operator detected.
+//	PostFix Expression: 7*22+
+//	Malformed expression - Invalid Postfix detected.
+//	PostFix Expression: A2/
+//	Malformed expression - Invalid operand/character detected.
+//	PostFix Expression: 72//
+//	Malformed expression - Invalid Postfix detected.
+//	PostFix Expression: (23-
+//	Malformed expression - Invalid Operator detected.
+//	
+//	ArrayStack Postfix Expression Evaluation:
+//	PostFix Expression: 345*+67*8+9*+
+//	The value is: 473
+//	PostFix Expression: 1234*+5/-
+//	The value is: -1
+//	PostFix Expression: 12+43-*
+//	The value is: 3
+//	PostFix Expression: 48+65-*32-22+*/
+//	The value is: 3
+//	PostFix Expression: 234+*
+//	The value is: 14
+//	PostFix Expression: 72/
+//	The value is: 3
+//	PostFix Expression: 512+4*+3-
+//	The value is: 14
+//	PostFix Expression: 72%
+//	Malformed expression - Invalid Operator detected.
+//	PostFix Expression: 7*22+
+//	Malformed expression - Invalid Postfix detected.
+//	PostFix Expression: A2/
+//	Malformed expression - Invalid operand/character detected.
+//	PostFix Expression: 72//
+//	Malformed expression - Invalid Postfix detected.
+//	PostFix Expression: (23-
+//	Malformed expression - Invalid Operator detected.
+//	
+//	Infix to Postfix Expression and Calculation:
+//	Infix expression: 3 + 4 * 5 + ( 6 * 7 + 8 ) * 9
+//	Converted Postfix: 345*+67*8+9*+
+//	The value is: 473
+//	Infix expression: 1 - (2 + 3 * 4)/5
+//	Converted Postfix: 1234*+5/-
+//	The value is: -1
+//	Infix expression: (1+2)*(4-3)
+//	Converted Postfix: 12+43-*
+//	The value is: 3
+//	Infix expression: (4+8)*(6-5)/((3-2)*(2+2))
+//	Converted Postfix: 48+65-*32-22+*/
+//	The value is: 3
+//	
+//	Manual Entry:
+//	Enter a Postfix Expression (i.e. 2 3 4 + * ) or q to go to Infix entry:
+//	234+*
+//	The value is: 14
+//	Enter a Postfix Expression (i.e. 2 3 4 + * ) or q to go to Infix entry:
+//	7 2%
+//	Malformed expression - Invalid Operator detected.
+//	Enter a Postfix Expression (i.e. 2 3 4 + * ) or q to go to Infix entry:
+//	45*2/
+//	The value is: 10
+//	Enter a Postfix Expression (i.e. 2 3 4 + * ) or q to go to Infix entry:
+//	q
+//	Enter an Infix Expression (i.e. 2 3 4 + * ) or q to quit:
+//	4+5*6
+//	Converted Postfix: 456*+
+//	The value is: 34
+//	Enter an Infix Expression (i.e. 2 3 4 + * ) or q to quit:
+//	(2+6)/(3-1)
+//	Converted Postfix: 26+31-/
+//	The value is: 4
+//	Enter an Infix Expression (i.e. 2 3 4 + * ) or q to quit:
+//	2*
+//	Converted Postfix: 2*
+//	Malformed expression - Invalid Postfix detected.
+//	Enter an Infix Expression (i.e. 2 3 4 + * ) or q to quit:
+//	q
+//	Thank you, goodbye!
+//	--------------------------------
+//	Process exited after 135 seconds with return value 0
+//	Press any key to continue . . .
